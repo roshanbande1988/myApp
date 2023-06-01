6ee4e1e443a24d381a97343dccb8269aa5e8c3d0
Roshan <roshan.bande@gmail.com>
2023-06-01 15:30:47 +0530

    my changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div>
-    <p>Hi this is my first react app </p>
-    <h1>I love you</h1>
+      <p>Hi this is my first react app </p>
+      <h1>I love you branch2</h1>
     </div>
   );
 }
